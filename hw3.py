a6e26c71bb5622c2e08d8d6bdb5128b2a5816f27
FFFFF0 <99376216+FFFFF0@users.noreply.github.com>
2022-03-01 10:01:57 +0500

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,23 @@
+for i in range(1, 101):
+   if 10 < i < 20:
+       print(i, "процентов")
+   elif i % 10 == 1:
+       print(i, "процент")
+   elif i % 10 == 2:
+       print(i, "процента")
+   elif i % 10 == 3:
+       print(i, "процента")
+   elif i % 10 == 4:
+       print(i, "процента")
+   elif i % 10 == 5:
+       print(i, "процентов")
+   elif i % 10 == 6:
+       print(i, "процентов")
+   elif i % 10 == 7:
+       print(i, "процентов")
+   elif i % 10 == 8:
+       print(i, "процентов")
+   elif i % 10 == 9:
+       print(i, "процентов")
+   elif i % 10 == 0:
+       print(i, "процентов")
